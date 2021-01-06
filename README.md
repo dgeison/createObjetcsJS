dfa23b701d19ab47d3191d6169c6eb76739cace6
Dgeison <paradg@gmail.com>
2021-01-06 18:18:58 -0300

    created readme and code block

---
+++ README.md
@@ -0,0 +1,14 @@
+# Create Objects in Javascripts
+
+## Crie um programa que armazena dados da empresa Rocketseat dentro de um objeto chamado empresa. Os dados a serem armazenados são:
+
+* Nome: Rocketseat
+
+* Cor: Roxo
+
+* Foco: Programação
+
+* Endereço:
+
+** Rua: Rua Guilherme Gembala
+** Número: 260
---
+++ empresa.js
@@ -0,0 +1,13 @@
+// contrução de objeto
+const empresa = {
+  nome: "RocketSeat",
+  cor: "Roxo",
+  endereco: {
+    rua: "Rua Guilherme Gembala",
+    numero: 260,
+  },
+}
+
+
+  // imprime em tela
+  console.log(`A empresa ${empresa.nome}, está localizada em ${empresa.endereco.rua}, ${empresa.endereco.numero}`)
